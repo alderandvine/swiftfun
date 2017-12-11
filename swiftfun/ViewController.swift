02953b003c50a797a3aab588ab484588d0a3306c
Heather Q Hay <heatherq@gmail.com>
2017-12-11 12:48:59 -0500

    12 11 17 1248

--- swiftfun/ViewController.swift
+++ swiftfun/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonCount = 0
+    @IBOutlet weak var test1: UILabel!
+    
+    @IBAction func action1(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        print (buttonCount)
+        
+        if buttonCount >= 10 {
+        
+        view.backgroundColor = UIColor.darkGray
+        test1.text = "you hit it enough!!"
+            
+        }
+        
+        if buttonCount >= 12 {
+            test1.text = "ok that's probably enough"
+        }
+        
+        if buttonCount >= 14 {
+            view.backgroundColor = UIColor.red
+            test1.text = "no seriously - stop it"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
 
     override func didReceiveMemoryWarning() {
